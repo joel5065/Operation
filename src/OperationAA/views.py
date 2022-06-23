1afc91e39e47a4ca158696db31891159417c6a95
joel5065 <kouakoujoel7@gmail.com>
2022-06-23 16:54:01 +0000

    commit

--- src/OperationAA/views.py
+++ src/OperationAA/views.py
@@ -6,5 +6,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+
 def login(request):
     return  render(request, "OperationAA/login.html", context={"prenom":"Joel"})
--- src/mainApp/views.py
+++ src/mainApp/views.py
@@ -1,6 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+    context = {'form' : form}
+    return render(request,'accounts/register.html', context)
 def homepage(request):
     return render(request,"mainApp/homepage.html")
 
