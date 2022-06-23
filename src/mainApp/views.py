d3c32b26636cda897a039bea1a01bc707d55ad47
joel5065 <kouakoujoel7@gmail.com>
2022-06-23 17:00:08 +0000

    first1

--- src/mainApp/views.py
+++ src/mainApp/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 
-
+# la vue pour enregister quelqu'un 
 def register(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
 
     context = {'form' : form}
     return render(request,'accounts/register.html', context)
+
+# la vue pour sur laquelle on tombe apres l'enregistrement
 def homepage(request):
     return render(request,"mainApp/homepage.html")
 
