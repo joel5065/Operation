a314e2cebae0c98f6f9bf5c70b0fd3fe411d365c
joel5065 <kouakoujoel7@gmail.com>
2022-06-29 15:22:03 +0000

    second commit

--- src/OperationAA/views.py
+++ src/OperationAA/views.py
@@ -1,24 +1,23 @@
-from django.shortcuts import  render
+from django.contrib import messages
+from django.shortcuts import  redirect, render
 from django.contrib.auth.forms import UserCreationForm
 
 
-def login(request):
-    form = UserCreationForm()
-    context = {'form':form}
-    return render(request,'login.html',context)
-
 
 def register(request):
-    form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,'Compte Cree')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'register.html', {'form':form})
 
-    context = {'form':form}
-    return render(request,'register.html', context)
 
-
+def login(request):
+    return render(request,'login.html')
 
 
 # def login(request):
